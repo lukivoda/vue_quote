2aac1ba26bc821c3c087e81126f752ccd3696361
Stevan Ristov <stevanris@gmail.com>
2017-11-28 20:18:31 +0100

    added NewQuote component and emiting to parent

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="container">
-
+    go prifacame emitiraniot event od child component-ot NewQuote i ja egzekutirame newQuote f-jata kako rezultat na prifacanjeto od cild-ot
+    <app-new-quote @quoteAdded="newQuote"> </app-new-quote>
    <app-quote-grid :quotesOk="quotes"></app-quote-grid>
   </div>
 </template>
 
 <script>
-  import QuoteGrid from './components/QuoteGrid.vue'
+  import QuoteGrid from './components/QuoteGrid.vue';
+  import NewQuote from './components/NewQuote.vue'
   export default {
 
     data() {
@@ -17,9 +19,19 @@
       }
 
     },
-
+//dodavame novi child components
     components: {
-      appQuoteGrid: QuoteGrid
+      appQuoteGrid: QuoteGrid,
+      appNewQuote: NewQuote
+    },
+
+    methods: {
+      //so ovaa f-ja go dodavame noviot quote
+      newQuote(quote){
+        if(quote !== '') {
+          this.quotes.unshift(quote.trim());
+        }
+      }
     }
 
   }
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,42 @@
+<template>
+  <form >
+  <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+    <label>Quote</label>
+    <!--double binding funkcionalnst na quote-->
+    <textarea class="form-control" rows="3" v-model="quote"></textarea>
+  </div>
+
+    <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+      <!--prevent go stavame za da  ne probuva da ja submitira formata(oti toa ne ni treba)-->
+      <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+    </div>
+
+  </form>
+</template>
+
+<script>
+
+  export default {
+
+    data() {
+      return {
+        quote: ''
+      }
+
+    },methods: {
+        createNew() {
+          //go emitirame content-ot od formata do parent component
+          this.$emit('quoteAdded',this.quote);
+          //ja praznime text area-ta
+          this.quote = '';
+
+        }
+
+    }
+  }
+
+</script>
+
+<style>
+
+</style>
